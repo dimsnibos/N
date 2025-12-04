8bfe2c01720654692d6ad02157ff78ef230e4848
dimsnibos <you@example.com>
2025-12-04 18:10:51 +0700

    okela

--- public/script.js
+++ public/script.js
@@ -1,8 +1,7 @@
-let namaLUU = prompt("masukan nama antum")
-if(namaLUU != "Dimas"){
-  alert("HII " + namaLUU+", Welcome to my website")
-  
-  const observer = new IntersectionObserver((entries) => {
+let namaLU = prompt("masukkan nama anda")
+if(namaLU != "DIMAS" && namaLU != "PUTRA"){
+  alert("Hi " +namaLU)
+    const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       if (entry.target.classList.contains("fade-left")) {
@@ -29,18 +28,21 @@ tengah.style.display = "block"
 hamburger.addEventListener("click",()=>{
   if(tengah.style.display === "block"){
     tengah.style.display = "none"
+    atas.style.position = "absolute"
     atas.style.rotate = "45deg"
-    atas.style.position ="absolute"
     bawah.style.rotate = "-45deg"
     navMenu.style.maxHeight = "500px"
   }else{
     tengah.style.display = "block"
+    atas.style.position = "relative"
     atas.style.rotate = "0deg"
     bawah.style.rotate = "0deg"
-    atas.style.position = "relative"
     navMenu.style.maxHeight = "0px"
   }
 })
+
+const load = document.getElementById("loading")
+const body = document.body
 
 const navbar = document.getElementById("navbar")
 window.onscroll = ()=>{
@@ -49,12 +51,9 @@ window.onscroll = ()=>{
   if(window.pageYOffset > fixedNav){
     navbar.style.boxShadow = "0 0 9px 0 black"
   }else{
-    navbar.style.boxShadow = "0 0 9px 0 black"
+    navbar.style.boxShadow = "none"
   }
 }
-
-const load = document.getElementById("loading")
-const body = document.body
 
 const ig = document.getElementById("ig")
 ig.addEventListener("click",()=>{
@@ -71,48 +70,48 @@ function whatssApp(){
   load.style.display = "flex"
   body.style.opacity = "0.7"
   setTimeout(()=>{
-  load.style.display = "none"
-  body.style.opacity = "1"
-  window.location.href = "https://wa.me/87763628863"
+    load.style.display = "none"
+    body.style.opacity = "1"
+    window.location.href = "https://wa.me/87763628863"
   },1200)
 }
 
-const kumpulin = document.getElementById("submit")
-kumpulin.onclick = function(){
-    load.style.display = "flex"
+const kumpul = document.getElementById("submit")
+kumpul.addEventListener("click",()=>{
+  load.style.display = "flex"
   body.style.opacity = "0.7"
   setTimeout(()=>{
     load.style.display = "none"
     body.style.opacity = "1"
   },1200)
-}
+})
 
 const value = document.getElementById("value")
 const calcRight = document.getElementById("calcRight")
 const calcLeft = document.getElementById("calcLeft")
 
-const numbers =["1","2","3","4","5","6","7","8","9","(","0",")"]
+const numbers = ["1","2","3","4","5","6","7","8","9","(","0",")"]
 numbers.forEach((number)=>{
   const button = document.createElement("button")
   button.innerHTML = number
-  button.addEventListener("click",()=>{
+  button.onclick = ()=>{
     value.innerHTML += number
-  })
+  }
   calcLeft.append(button)
 })
 
-const operations = ["+","-","x","/"]
+const operations =["+","-","/","x"]
 operations.forEach((operation)=>{
   const button = document.createElement("button")
   button.innerHTML = operation
-  button.onclick = ()=>{
-    value.innerHTML+= operation
-  }
+  button.addEventListener("click",()=>{
+    value.innerHTML += operation
+  })
   calcRight.append(button)
 })
 
 const enter = document.createElement("button")
-enter.innerHTML ="ENTER"
+enter.innerHTML = "ENTER"
 enter.addEventListener("click",()=>{
   let ekspresi = value.innerHTML
   ekspresi = ekspresi.replace(/x/g,"*")
@@ -120,9 +119,9 @@ enter.addEventListener("click",()=>{
     value.innerHTML = eval(ekspresi)
   }
   catch{
-    value.innerHTMl = "ERROR"
+    value.innerHTML = "ERROR OPERATIONS"
     setTimeout(()=>{
-      value.innerHTML = " "
+      value.innerHTML =""
     },1200)
   }
 })
@@ -140,7 +139,7 @@ clear.innerHTML = "CLEAR"
 clear.addEventListener("click",()=>{
   value.innerHTML = "CLEAR THE AREA"
   setTimeout(()=>{
-    value.innerHTML = " "
+    value.innerHTML = ""
   },1200)
 })
 calcRight.append(clear)
@@ -149,8 +148,9 @@ const bgr = document.createElement("button")
 bgr.innerHTML = "BGR"
 calcRight.append(bgr)
 
+
 const piano = document.getElementById("chord")
-const chords = ["a","c","d","e","f"]
+const chords = [ "a","c","d","e","f"]
 chords.forEach((chord)=>{
   const button = document.createElement("button")
   button.innerHTML = chord
@@ -161,46 +161,46 @@ chords.forEach((chord)=>{
   piano.append(button)
 })
 
+
 const user = document.getElementById("userTanya")
 const bot = document.getElementById("botJawab")
 
 function botSay(data){
   return[
-    `Hello, Im jarvis, who are yu?`,
+    `Hi Im jarvis, whats your name?`,
     `Hi ${data?.nama}, where are you from?`,
     `Ouhh youre from ${data?.kota}, whats your hobby?`,
-    `Wow ${data?.hobi}, thats Interesting!`,
+    `Wiw ${data?.hobi}, thats interesting!`
   ]
 }
 
-let init = 0
 let userData = [ ]
+let init = 0
 bot.innerHTML = botSay()[0]
 
 function submit(){
   init++
-  if(init === 1){
+  if(init ===1){
     responseBot({nama : user.value})
   }
   else if (init === 2){
-    responseBot({kota : user.value})
+    responseBot({kota:user.value})
   }
   else if(init === 3){
     responseBot({hobi : user.value})
     user.style.display = "none"
   }
-  else if(init ===4){
-    bot.innerHTML = "Nice to know Ya !!"
+  else if(init === 4){
+    bot.innerHTML = "Nice to know ya Pal !"
     setTimeout(()=>{
       load.style.display = "flex"
       body.style.opacity = "0.7"
-    },400)
-
+    },500)
     setTimeout(()=>{
       load.style.display = "none"
       body.style.opacity = "1"
       window.location.reload()
-    },1600)
+    },1200)
   }
 }
 
@@ -211,11 +211,11 @@ function responseBot(jawabanUser){
   setTimeout(()=>{
     load.style.display = "none"
     body.style.opacity = "1"
-    user.value = " "
     bot.innerHTML = botSay(jawabanUser)[init]
+    user.value = " "
   },1200)
 }
 }else{
-  alert("KEJAM LU " + namaLUU)
+  alert("WRONG TETOT")
   window.location.reload()
 }
