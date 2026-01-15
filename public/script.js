1bbfcfe04dec5a33177f36591d05862aa1165b99
dimasputramadian <madianbussines@gmail.com>
2026-01-15 22:19:18 +0700

    neq

--- public/script.js
+++ public/script.js
@@ -1,6 +1,6 @@
-let nm = prompt("Nama mu?")
-if (nm != "dimsuy" || nm != "DIMSS") {
-  alert("HII " + nm + ", Welcome")
+let nama1 = prompt("Masukkan nama mu")
+if (nama1 != "Dimsuy" || nama1 != "DIMASUY") {
+  alert("HII " + nama1 + ", WELCOME!")
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
@@ -21,18 +21,18 @@ if (nm != "dimsuy" || nm != "DIMSS") {
 
   const hamburger = document.getElementById("hamburger")
   const atas = document.getElementById("atas")
-  const tengah = document.getElementById("tengah")
   const bawah = document.getElementById("bawah")
+  const tengah = document.getElementById("tengah")
   const navMenu = document.getElementById("nav-menu")
-
   tengah.style.display = "block"
+
   hamburger.addEventListener("click", () => {
     if (tengah.style.display === "block") {
       tengah.style.display = "none"
       atas.style.position = "absolute"
       atas.style.rotate = "45deg"
-      bawah.style.rotate = "-45deg"
       navMenu.style.maxHeight = "500px"
+      bawah.style.rotate = "-45deg"
     } else {
       tengah.style.display = "block"
       atas.style.position = "relative"
@@ -47,12 +47,12 @@ if (nm != "dimsuy" || nm != "DIMSS") {
 
   const navbar = document.getElementById("navbar")
   window.addEventListener("scroll", () => {
-    const header = document.querySelector("header")
+    const header = document.querySelector("header")                       //local scope dalam fungsi dan hanya bisa digunakan dalam fungsi
     const fixedNav = header.offsetTop
     if (window.pageYOffset > fixedNav) {
       navbar.style.boxShadow = "0 0 9px 0 black"
     } else {
-      navbar.style.boxShadow = "none"
+      navbar.style.boxShadow = "0px"
     }
   })
 
@@ -80,7 +80,7 @@ if (nm != "dimsuy" || nm != "DIMSS") {
 
   function whatssApp() {
     load.style.display = "flex"
-    body.style.opacity = "0.7"
+    body.style.opcity = "0.7"
     setTimeout(() => {
       load.style.display = "none"
       body.style.opacity = "1"
@@ -94,16 +94,16 @@ if (nm != "dimsuy" || nm != "DIMSS") {
     body.style.opacity = "0.7"
     setTimeout(() => {
       load.style.display = "none"
-      body.style.opcity = "1"
+      body.style.opacity = "1"
     }, 1200)
   })
 
-  const calcLeft = document.getElementById("calcLeft")
   const calcRight = document.getElementById("calcRight")
+  const calcLeft = document.getElementById("calcLeft")
   const value = document.getElementById("value")
 
   const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "(", "0", ")"]
-  numbers.forEach((number) => {
+  numbers.forEach((number) => {  //forEach untuk meloop elemen dalam array
     const button = document.createElement("button")
     button.innerHTML = number
     button.addEventListener("click", () => {
@@ -125,18 +125,22 @@ if (nm != "dimsuy" || nm != "DIMSS") {
   const enter = document.createElement("button")
   enter.innerHTML = "ENTER"
   enter.addEventListener("click", () => {
-    let ekspresi = value.innerHTML
-    ekspresi = ekspresi.replace("x", "*")  //ganti x dengan * biar kali
+    let nilai = value.innerHTML
+    nilai = nilai.replace("x", "*")
     try {
-      value.innerHTML = eval(ekspresi)
-    } catch (err) {
+      value.innerHTML = eval(nilai)
+    } catch {
       value.innerHTML = "ERROR"
       setTimeout(() => {
-        value.innerHTML = ""
+        value.innerHTML = " "
       }, 1200)
     }
   })
   calcRight.append(enter)
+
+  const bgr = document.createElement("button")
+  bgr.innerHTML = "BGR"
+  calcRight.append(bgr)
 
   const del = document.createElement("button")
   del.innerHTML = "DEL"
@@ -148,24 +152,21 @@ if (nm != "dimsuy" || nm != "DIMSS") {
   const clear = document.createElement("button")
   clear.innerHTML = "CLEAR"
   clear.addEventListener("click", () => {
-    value.innerHTML = "CLEAR THE AREA"
+    value.innerHTML = "CLEAR THE AREA..."
     setTimeout(() => {
       value.innerHTML = ""
     }, 1200)
   })
   calcRight.append(clear)
 
-  const bgr = document.createElement("button")
-  bgr.innerHTML = "BGR"
-  calcRight.append(bgr)
-
   const piano = document.getElementById("chord")
+
   const chords = ["a", "c", "d", "e", "f"]
-  chords.forEach((c) => {
+  chords.forEach((chord) => {
     const button = document.createElement("button")
-    button.innerHTML = c
+    button.innerHTML = chord
     button.addEventListener("click", () => {
-      const sound = new Audio(`${c}.wav`)
+      const sound = new Audio(`${chord}.wav`)
       sound.play()
     })
     piano.append(button)
@@ -176,9 +177,9 @@ if (nm != "dimsuy" || nm != "DIMSS") {
 
   function botSay(data) {
     return [
-      "Hi Im DIMSUY who are you?",
+      `Hi im dims, who are you?`,
       `Hi ${data?.nama}, where are you from?`,
-      `Ohh youre from ${data?.kota}, whats your hobby?`,
+      `Ouh youre from ${data?.kota}, whats your hobby?`,
       `Wow ${data?.hobi}, such an interesting hobby!`
     ]
   }
@@ -188,28 +189,26 @@ if (nm != "dimsuy" || nm != "DIMSS") {
   bot.innerHTML = botSay()[0]
 
   function submit() {
-    init = init + 1
+    init++
     if (init === 1) {
       responseBot({ nama: user.value })
     }
     else if (init === 2) {
       responseBot({ kota: user.value })
-    }
-    else if (init === 3) {
+    } else if (init === 3) {
       responseBot({ hobi: user.value })
       user.style.display = "none"
-    }
-    else if (init === 4) {
-      bot.innerHTML = "NICE TO KNOW YA!"
+    } else if (init === 4) {
+      bot.innerHTML = "Nice to Know Ya!"
       setTimeout(() => {
         load.style.display = "flex"
         body.style.opacity = "0.7"
-      }, 500)
+      }, 1000)
       setTimeout(() => {
         load.style.display = "none"
         body.style.opacity = "1"
         window.location.reload()
-      }, 2000)
+      }, 3000)
     }
   }
 
@@ -220,10 +219,12 @@ if (nm != "dimsuy" || nm != "DIMSS") {
     setTimeout(() => {
       load.style.display = "none"
       body.style.opacity = "1"
-      bot.innerHTML = botSay(jawabanUser)[init]
       user.value = ""
+      bot.innerHTML = botSay(jawabanUser)[init]
     }, 1200)
   }
+
+
 } else {
-  alert("NONO YA" + nm)
+  alert("NO NO YA " + nama1)
 }
